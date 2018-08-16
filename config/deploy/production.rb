be699dcad3027450c06417bbe9edadc1720534c9
benson <benson.liao@anker.com>
2018-08-16 11:25:21 +0800

    增加了服务器密码

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -64,4 +64,4 @@ set :rails_env, :production
 set :deploy_to, "/usr/local/project_name"
 set :repo_url, "REPO_URL"
 set :branch, ENV["PRODUCTION_BRANCH"]
-server ENV["PRODUCTION_SERVER_IP"], user: ENV["PRODUCTION_DEPLOY_USER"], roles: %w{web app db}
+server ENV["PRODUCTION_SERVER_IP"], user: ENV["PRODUCTION_DEPLOY_USER"],password: '23818213ljh', roles: %w{web app db}
