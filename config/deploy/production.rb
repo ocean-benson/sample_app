50ef7070e77ace3cbed910b2cc9d14f56f1d38bf
benson <benson.liao@anker.com>
2018-08-16 14:46:08 +0800

    修改了部署文件夹

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -61,7 +61,7 @@
 #   }
 set :stage, :production
 set :rails_env, :production
-set :deploy_to, "/home/local/apps/sample_app"
+set :deploy_to, "/home/deploy/apps/sample_app"
 #set :repo_url, "REPO_URL"
 set :branch, ENV["PRODUCTION_BRANCH"]
 server ENV["PRODUCTION_SERVER_IP"], user: ENV["PRODUCTION_DEPLOY_USER"], roles: %w{web app db}
